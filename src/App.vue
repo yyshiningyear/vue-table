4b8de50475fec4a1b1ae399b176c5260282b9161
yangyong <yyshiningyear@gmail.com>
2018-09-02 19:04:28 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <vue-table
+      :columns='columns'
+      :tableData='tableData'>
+    </vue-table>
+  </div>
+</template>
+
+<script>
+import VueTable from './components/VueTable'
+
+export default {
+  name: 'App',
+  components: {
+    VueTable
+  },
+  data() {
+    return {
+      columns: [
+        {
+          key: 'no',
+          name: 'No',
+          width: 80,
+        }, {
+          key: 'col1',
+          name: '第一列',
+          width: 200,
+        }, {
+          key: 'col2',
+          name: '第二列',
+          width: 200,
+        }, {
+          key: 'col3',
+          name: '第三列',
+          width: 200,
+        }, {
+          key: 'col4',
+          name: '第四列',
+          width: 200,
+        }, {
+          key: 'col5',
+          name: '第五列',
+          width: 200,
+        }, {
+          key: 'col6',
+          name: '第六列',
+          width: 200,
+        }, {
+          key: 'col7',
+          name: '第七列',
+          width: 200,
+        }
+      ],
+      tableData: []
+    }
+  },
+  beforeMount() {
+    this.initData();
+  },
+  methods: {
+    initData() {
+      const dataSize = 100000;
+      const dataList = [];
+      for (let i = 1; i <= dataSize; i++) {
+        const data = {
+          no: i
+        };
+        for (let j = 1; j <=7; j++) {
+          data[`col${j}`] = `第${i}行第${j}列`
+        }
+        dataList.push(data);
+      }
+
+      this.tableData = dataList;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 800px;
+  height: 500px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/VueTable.vue
@@ -0,0 +1,139 @@
+<template>
+  <!-- wrapper用于触发滚动条 -->
+  <div ref="wrapper" class="vue-table-wrapper" @scroll="handleScroll">
+    <div v-if="columns.length > 0" class="vue-table" :style="{width: tableWidth+'px'}">
+        <div class="vue-table-header-wrapper">
+          <table border="0" class="vue-table-fixed-header">
+            <colgroup>
+              <col :width="columns[0].width">
+            </colgroup>
+            <tr :style="{height: headerHeight + 'px'}">
+              <th>
+                <span>{{columns[0].name}}</span>
+              </th>
+            </tr>
+          </table>
+          <table border="0" class="vue-table-header">
+            <colgroup>
+              <col v-for="col in columns" :key="col.key" :width="col.width">
+            </colgroup>
+            <tr :style="{height: headerHeight + 'px'}">
+              <th v-for="col in columns" :key="col.key">
+                <span>{{col.name}}</span>
+              </th>
+            </tr>
+          </table>
+        </div>
+        <div class="vue-table-content">
+          <div :style="{height: topMarginHeight+'px'}"></div>
+          <div :style="{width: tableWidth+'px', position: 'relative'}">
+            <table border="0" class="vue-table-fixed-table">
+              <colgroup>
+                <col :width="columns[0].width">
+              </colgroup>
+              <tbody>
+                <tr v-for="(data, index) in showTableData"
+                  :key="'fixedCol' + index"
+                  :style="{height: rowHeight + 'px'}">
+                  <td>
+                    <div class="vue-table-cell">{{data[columns[0].key]}}</div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table border="0" class="vue-table-content-table">
+              <colgroup>
+                <col v-for="col in columns" :key="col.key" :width="col.width">
+              </colgroup>
+              <tbody>
+                <tr v-for="(data, index) in showTableData"
+                  :key="'data' + index"
+                  :style="{height: rowHeight + 'px'}">
+                  <td v-for="col in columns" :key="'data' + index + col.key">
+                    <div class="vue-table-cell">{{data[col.key]}}</div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div :style="{height: bottomMarginHeight + 'px'}"></div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueTable",
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+      default: []
+    },
+    tableData: {
+      type: Array,
+      required: true,
+      default: []
+    }
+  },
+  data() {
+    return {
+      headerHeight: 50,
+      rowHeight: 48, // TODO 先写死行高
+      showHeight: 500, // 可视的宽高 先写死
+      showWidth: 800,
+      scrollTop: 0,
+      shouldHandleScroll: true // 防止频繁触发滚动
+    };
+  },
+  computed: {
+    tableWidth() {
+      let width = 0;
+      this.columns.forEach(col => (width += col.width));
+      return width;
+    },
+    tableHeight() {
+      return this.tableData.length * this.rowHeight;
+    },
+    // 可视范围内的数据
+    startIndex() {
+      return Math.floor(this.scrollTop / this.rowHeight);
+    },
+    endIndex() {
+      return this.startIndex + Math.ceil(this.showHeight / this.rowHeight);
+    },
+    showTableData() {
+      return this.tableData.slice(this.startIndex, this.endIndex);
+    },
+    topMarginHeight() {
+      return this.startIndex * this.rowHeight;
+    },
+    bottomMarginHeight() {
+      return this.tableHeight - this.endIndex * this.rowHeight;
+    }
+  },
+  watch: {
+    scrollTop() {
+      this.shouldHandleScroll = false;
+      this.$nextTick(() => {
+        this.shouldHandleScroll = true;
+        this.$refs["wrapper"].scrollTop = this.scrollTop;
+      });
+    }
+  },
+  methods: {
+    handleScroll(e) {
+      if (!this.shouldHandleScroll) {
+        return;
+      }
+      let ele = e.srcElement || e.target;
+      this.scrollTop = ele.scrollTop;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "./table.less";
+</style>
